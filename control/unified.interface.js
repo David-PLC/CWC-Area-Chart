dfa5e9fc0ee9a8bc70a853fd16cf73ec8106fe5b
Davide <72767272+David-PLC@users.noreply.github.com>
2023-04-17 07:44:16 +0200

    Add files via upload

---
+++ control/unified.interface.js
@@ -0,0 +1,49 @@
+const _extensions = ['HMI'];
+const _timeout = 1000;
+const _contract = {
+    methods: {},
+    events: [],
+    properties: {
+        'X_axis': [],
+        'Y_axis': [],
+        'DataName': [],
+        'SeriesColor': [],
+        'X_axisname': '',
+        'Y_axisname': '',
+        'SeriesLabels': ''
+    }
+};
+
+var UnifiedInterface = function () {
+    var _initialize = function () {
+        console.log('COMI-Chart: UnifiedInterface initialized');
+    };
+    
+    var _setProps = function (data) {
+         console.log('COMI-Cart: Key <', data.key, '>');
+         if (data.key === 'Y_axis' || data.key === 'DataName') {
+            console.log('updateSeries!');
+            updateSeries(WebCC.Properties.DataName, WebCC.Properties.Y_axis);
+        } else {
+         chartInit(WebCC.Properties);
+        }
+    };
+
+    return {
+        Local: {
+            initialize: _initialize,
+            setProps: _setProps
+        },
+        HostListener: _contract
+    };
+}();
+
+var unifiedInterfaceInit = function () {
+    // Initialize 
+    UnifiedInterface.Local.initialize();
+
+    // Subscribe for value changes
+    if (WebCC && WebCC.onPropertyChanged) {
+        WebCC.onPropertyChanged.subscribe(UnifiedInterface.Local.setProps);
+    }
+};
